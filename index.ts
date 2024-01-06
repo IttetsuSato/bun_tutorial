e4d3b3cc3f71d61cf06f9926293b09f5f123b4b4
Ittetsu Sato <ittetsusato@gmail.com>
2024-01-06 14:57:37 +0900

    install ASCII

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 3000,
+  fetch() {
+    const body = figlet.textSync("Hello, World!");
+    return new Response(body);
+  },
+});
+console.log(Bun.version);
+
+console.log(`Listening on http://localhost:${server.port} ...`);
